672b8128cc71518414babb6acfda1694780c8c0d
Jens Stalder <jens.stalder@casasoft.ch>
2019-01-31 17:40:03 +0100

    declare prop

--- src/CasasoftStandards/src/CasasoftStandards/Service/HeatService.php
+++ src/CasasoftStandards/src/CasasoftStandards/Service/HeatService.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
 class HeatService {
 
     public $items = [];
+    private $groups = null;
 
     public function __construct($translator){
         $this->translator = $translator;
